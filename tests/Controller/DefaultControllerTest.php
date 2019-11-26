84d1d208b35e30f20f01cc49fc62f782ac050f12
Michiel Kodde <mkodde@ibuildings.nl>
2019-11-26 10:09:30 +0100

    Apply changes to make the build pass
    1. The composer file was not able to pass `composer validate`. The name
    was not lower cased, and the lockfile was not in sync. In the process,
    some minor updates where installed for several dependencies
    2. The controller web test was not in sync, the updated translations
    where not updated in the test

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -28,6 +28,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertStringContainsString('Welcome to the Generic SAML Stepup Provider SDK', $crawler->filter('h2')->text());
+        $this->assertStringContainsString('Welcome to the Azure MFA GSSP', $crawler->filter('h2')->text());
     }
 }
