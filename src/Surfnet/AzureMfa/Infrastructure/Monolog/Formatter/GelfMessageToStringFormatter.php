7bf2ec5c1fdaa28e1376208fc842959c5a2b5a64
Michiel Kodde <mkodde@ibuildings.nl>
2020-01-14 08:38:03 +0100

    Format syslog messages
    The GelfMessageToStringFormatter is used from other steup projects. It logs in
    json format to syslog.

---
+++ src/Surfnet/AzureMfa/Infrastructure/Monolog/Formatter/GelfMessageToStringFormatter.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Copyright 2020 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\AzureMfa\Infrastructure\Monolog\Formatter;
+
+
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Formatter\GelfMessageFormatter;
+
+class GelfMessageToStringFormatter implements FormatterInterface
+{
+    /**
+     * @var GelfMessageFormatter
+     */
+    private $formatter;
+
+    /**
+     * @param GelfMessageFormatter $formatter
+     */
+    public function __construct(GelfMessageFormatter $formatter)
+    {
+        $this->formatter = $formatter;
+    }
+
+    /**
+     * {@inheritdoc} the gelf message is an array which cannot be logged into a single line
+     * By jsonencoding the message we can write it on a single line
+     */
+    public function format(array $record): string
+    {
+        $message = $this->formatter->format($record);
+
+        // we need to keep the last new line, otherwise everything is appended on the same line :)
+        $message->setFullMessage(str_replace("\n", ', ', $message->getFullMessage()) . "\n");
+
+        return json_encode($message->toArray());
+    }
+
+    /**
+     * Formats a set of log records.
+     *
+     * @param  array $records A set of records to format
+     * @return mixed The formatted set of records
+     */
+    public function formatBatch(array $records): array
+    {
+        return array_map([$this, 'format'], $records);
+    }
+}
